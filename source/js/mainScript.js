3d680cfa40ea04bdcda2e8f9da6ae7970b76c2c4
Dorzhi <dorzhi.gombozhapov@yandex.ru>
2022-02-04 02:00:27 +0900

    Ready phone ver

--- source/js/mainScript.js
+++ source/js/mainScript.js
@@ -1,4 +1,4 @@
-    // Введение
+    // Введение телефон
     //Основные термины
 
     $('#mainTermin').on('click', function(e)
@@ -62,6 +62,88 @@
 
     // Программирование микроконтролеров
     $('#programmerControllers').on('click', function(e)
+    {
+        e.preventDefault();
+        var data = $(this).serialize();
+        $.ajax
+        ({
+            url: './pages/foreword/programmerControllers.html', 
+            type: 'GET',
+            success: function(responce)
+            {        
+                $('#ShowPage').html(responce);
+            },
+            error: function()
+            {
+                alert('Error!');            
+            }
+        });
+    });
+
+    //Основной
+    //Основные термины
+
+    $('#mainTermin2').on('click', function(e)
+    {
+        e.preventDefault();
+        var data = $(this).serialize();
+        $.ajax
+        ({
+            url: './pages/foreword/mainTermin.html', 
+            type: 'GET',
+            success: function(responce)
+            {        
+                $('#ShowPage').html(responce); 
+            },
+            error: function()
+            {
+                alert('Error!');            
+            }
+        });
+    });
+
+    // Компоненты встроенных систем
+    $('#systemComponents2').on('click', function(e)
+    {
+        e.preventDefault();
+        var data = $(this).serialize();
+        $.ajax
+        ({
+            url: './pages/foreword/systemComponents.html', 
+            type: 'GET',
+            success: function(responce)
+            {        
+                $('#ShowPage').html(responce); 
+            },
+            error: function()
+            {
+                alert('Error!');            
+            }
+        });
+    });
+
+    // Микроконтроллеры
+    $('#microControllers2').on('click', function(e)
+    {
+        e.preventDefault();
+        var data = $(this).serialize();
+        $.ajax
+        ({
+            url: './pages/foreword/microControllers.html', 
+            type: 'GET',
+            success: function(responce)
+            {        
+                $('#ShowPage').html(responce);
+            },
+            error: function()
+            {
+                alert('Error!');            
+            }
+        });
+    });
+
+    // Программирование микроконтролеров
+    $('#programmerControllers2').on('click', function(e)
     {
         e.preventDefault();
         var data = $(this).serialize();
