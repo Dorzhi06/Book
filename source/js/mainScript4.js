50e16f782836ff0394db47b7238b35c000c548be
Dorzhi <dorzhi.gombozhapov@yandex.ru>
2022-02-04 10:43:48 +0900

    Ready 82%

--- source/js/mainScript4.js
+++ source/js/mainScript4.js
@@ -36,6 +36,82 @@ $('#2Quest').on('click', function(e)
     });
 });
 
+$('#3Quest').on('click', function(e)
+{
+    e.preventDefault();
+    var data = $(this).serialize();
+    $.ajax
+    ({
+        url: './pages/Round_4/3Quest.html', 
+        type: 'GET',
+        success: function(responce)
+        {        
+            $('#ShowPage').html(responce); 
+        },
+        error: function()
+        {
+            alert('Error!');            
+        }
+    });
+});
+
+$('#4Quest').on('click', function(e)
+{
+    e.preventDefault();
+    var data = $(this).serialize();
+    $.ajax
+    ({
+        url: './pages/Round_4/4Quest.html', 
+        type: 'GET',
+        success: function(responce)
+        {        
+            $('#ShowPage').html(responce); 
+        },
+        error: function()
+        {
+            alert('Error!');            
+        }
+    });
+});
+
+$('#5Quest').on('click', function(e)
+{
+    e.preventDefault();
+    var data = $(this).serialize();
+    $.ajax
+    ({
+        url: './pages/Round_4/5Quest.html', 
+        type: 'GET',
+        success: function(responce)
+        {        
+            $('#ShowPage').html(responce); 
+        },
+        error: function()
+        {
+            alert('Error!');            
+        }
+    });
+});
+
+$('#6Quest').on('click', function(e)
+{
+    e.preventDefault();
+    var data = $(this).serialize();
+    $.ajax
+    ({
+        url: './pages/Round_4/6Quest.html', 
+        type: 'GET',
+        success: function(responce)
+        {        
+            $('#ShowPage').html(responce); 
+        },
+        error: function()
+        {
+            alert('Error!');            
+        }
+    });
+});
+
 $('#1Quest2').on('click', function(e)
 {
     e.preventDefault();
@@ -74,3 +150,78 @@ $('#2Quest2').on('click', function(e)
     });
 });
 
+$('#3Quest2').on('click', function(e)
+{
+    e.preventDefault();
+    var data = $(this).serialize();
+    $.ajax
+    ({
+        url: './pages/Round_4/3Quest.html', 
+        type: 'GET',
+        success: function(responce)
+        {        
+            $('#ShowPage').html(responce); 
+        },
+        error: function()
+        {
+            alert('Error!');            
+        }
+    });
+});
+
+$('#4Quest2').on('click', function(e)
+{
+    e.preventDefault();
+    var data = $(this).serialize();
+    $.ajax
+    ({
+        url: './pages/Round_4/4Quest.html', 
+        type: 'GET',
+        success: function(responce)
+        {        
+            $('#ShowPage').html(responce); 
+        },
+        error: function()
+        {
+            alert('Error!');            
+        }
+    });
+});
+
+$('#5Quest2').on('click', function(e)
+{
+    e.preventDefault();
+    var data = $(this).serialize();
+    $.ajax
+    ({
+        url: './pages/Round_4/5Quest.html', 
+        type: 'GET',
+        success: function(responce)
+        {        
+            $('#ShowPage').html(responce); 
+        },
+        error: function()
+        {
+            alert('Error!');            
+        }
+    });
+});
+
+$('#6Quest2').on('click', function(e)
+{
+    e.preventDefault();
+    var data = $(this).serialize();
+    $.ajax
+    ({
+        url: './pages/Round_4/6Quest.html', 
+        type: 'GET',
+        success: function(responce)
+        {        
+            $('#ShowPage').html(responce); 
+        },
+        error: function()
+        {
+            alert('Error!');            
+        }
+    });
+});
