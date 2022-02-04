2f19d7f94874870cefe171fcf79aff316f22c167
Dorzhi <dorzhi.gombozhapov@yandex.ru>
2022-02-04 10:40:58 +0900

    Ready 80% site

--- source/js/mainScript3.js
+++ source/js/mainScript3.js
@@ -36,25 +36,6 @@
         });
     });
 
-    $('#Example').on('click', function(e)
-    {
-        e.preventDefault();
-        var data = $(this).serialize();
-        $.ajax
-        ({
-            url: './pages/Round_3/3Example.html', 
-            type: 'GET',
-            success: function(responce)
-            {        
-                $('#ShowPage').html(responce); 
-            },
-            error: function()
-            {
-                alert('Error!');            
-            }
-        });
-    });
-
     $('#processing2').on('click', function(e)
     {
         e.preventDefault();
@@ -81,25 +62,6 @@
         $.ajax
         ({
             url: './pages/Round_3/2combo.html', 
-            type: 'GET',
-            success: function(responce)
-            {        
-                $('#ShowPage').html(responce); 
-            },
-            error: function()
-            {
-                alert('Error!');            
-            }
-        });
-    });
-
-    $('#Example2').on('click', function(e)
-    {
-        e.preventDefault();
-        var data = $(this).serialize();
-        $.ajax
-        ({
-            url: './pages/Round_3/3Example.html', 
             type: 'GET',
             success: function(responce)
             {        
