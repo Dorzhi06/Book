3b7f00b9b4fe7d8f1f55a7285130bec72a6031fb
Dorzhi <dorzhi.gombozhapov@yandex.ru>
2022-02-10 05:33:31 +0900

    Start edit 3 rount new data

--- source/js/mainScript3.js
+++ source/js/mainScript3.js
@@ -1,10 +1,10 @@
-    $('#processing').on('click', function(e)
+    $('#mainFunction').on('click', function(e)
     {
         e.preventDefault();
         var data = $(this).serialize();
         $.ajax
         ({
-            url: './pages/Round_3/1processing.html', 
+            url: './pages/Round_3/1_mainFunction.html', 
             type: 'GET',
             success: function(responce)
             {        
@@ -17,13 +17,13 @@
         });
     });
 
-    $('#combo').on('click', function(e)
+    $('#massiveAndString').on('click', function(e)
     {
         e.preventDefault();
         var data = $(this).serialize();
         $.ajax
         ({
-            url: './pages/Round_3/2combo.html', 
+            url: './pages/Round_3/2_massiveAndString.html', 
             type: 'GET',
             success: function(responce)
             {        
@@ -36,13 +36,13 @@
         });
     });
 
-    $('#processing2').on('click', function(e)
+    $('#inputAndOutput').on('click', function(e)
     {
         e.preventDefault();
         var data = $(this).serialize();
         $.ajax
         ({
-            url: './pages/Round_3/1processing.html', 
+            url: './pages/Round_3/3_inputAndOutput.html', 
             type: 'GET',
             success: function(responce)
             {        
@@ -55,13 +55,167 @@
         });
     });
 
-    $('#combo2').on('click', function(e)
+    $('#writeData').on('click', function(e)
     {
         e.preventDefault();
         var data = $(this).serialize();
         $.ajax
         ({
-            url: './pages/Round_3/2combo.html', 
+            url: './pages/Round_3/4_writeData.html', 
+            type: 'GET',
+            success: function(responce)
+            {        
+                $('#ShowPage').html(responce); 
+            },
+            error: function()
+            {
+                alert('Error!');            
+            }
+        });
+    });
+
+    $('#jopWithDisp').on('click', function(e)
+    {
+        e.preventDefault();
+        var data = $(this).serialize();
+        $.ajax
+        ({
+            url: './pages/Round_3/5_jopWithDisp.html', 
+            type: 'GET',
+            success: function(responce)
+            {        
+                $('#ShowPage').html(responce); 
+            },
+            error: function()
+            {
+                alert('Error!');            
+            }
+        });
+    });
+
+    $('#standartBibl').on('click', function(e)
+    {
+        e.preventDefault();
+        var data = $(this).serialize();
+        $.ajax
+        ({
+            url: './pages/Round_3/6_standartBibl.html', 
+            type: 'GET',
+            success: function(responce)
+            {        
+                $('#ShowPage').html(responce); 
+            },
+            error: function()
+            {
+                alert('Error!');            
+            }
+        });
+    });
+
+    // Версия для ПК
+
+    $('#mainFunction2').on('click', function(e)
+    {
+        e.preventDefault();
+        var data = $(this).serialize();
+        $.ajax
+        ({
+            url: './pages/Round_3/1_mainFunction.html', 
+            type: 'GET',
+            success: function(responce)
+            {        
+                $('#ShowPage').html(responce); 
+            },
+            error: function()
+            {
+                alert('Error!');            
+            }
+        });
+    });
+
+    $('#massiveAndString2').on('click', function(e)
+    {
+        e.preventDefault();
+        var data = $(this).serialize();
+        $.ajax
+        ({
+            url: './pages/Round_3/2_massiveAndString.html', 
+            type: 'GET',
+            success: function(responce)
+            {        
+                $('#ShowPage').html(responce); 
+            },
+            error: function()
+            {
+                alert('Error!');            
+            }
+        });
+    });
+
+    $('#inputAndOutput2').on('click', function(e)
+    {
+        e.preventDefault();
+        var data = $(this).serialize();
+        $.ajax
+        ({
+            url: './pages/Round_3/3_inputAndOutput.html', 
+            type: 'GET',
+            success: function(responce)
+            {        
+                $('#ShowPage').html(responce); 
+            },
+            error: function()
+            {
+                alert('Error!');            
+            }
+        });
+    });
+
+    $('#writeData2').on('click', function(e)
+    {
+        e.preventDefault();
+        var data = $(this).serialize();
+        $.ajax
+        ({
+            url: './pages/Round_3/4_writeData.html', 
+            type: 'GET',
+            success: function(responce)
+            {        
+                $('#ShowPage').html(responce); 
+            },
+            error: function()
+            {
+                alert('Error!');            
+            }
+        });
+    });
+
+    $('#jopWithDisp2').on('click', function(e)
+    {
+        e.preventDefault();
+        var data = $(this).serialize();
+        $.ajax
+        ({
+            url: './pages/Round_3/5_jopWithDisp.html', 
+            type: 'GET',
+            success: function(responce)
+            {        
+                $('#ShowPage').html(responce); 
+            },
+            error: function()
+            {
+                alert('Error!');            
+            }
+        });
+    });
+
+    $('#standartBibl2').on('click', function(e)
+    {
+        e.preventDefault();
+        var data = $(this).serialize();
+        $.ajax
+        ({
+            url: './pages/Round_3/6_standartBibl.html', 
             type: 'GET',
             success: function(responce)
             {        
